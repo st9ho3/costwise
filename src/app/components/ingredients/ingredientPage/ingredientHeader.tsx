9e401ca570c51ea129bdea43027777d0b872902b
st9ho3 <stahos21@gmail.com>
2025-11-03 21:24:04 +0200

    feat: Fixed a build issue with icon container

--- src/app/components/ingredients/ingredientPage/ingredientHeader.tsx
+++ src/app/components/ingredients/ingredientPage/ingredientHeader.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Title from '@/app/components/ingredients/ingredientPage/title'
-import Icon from '@/app/components/ingredients/ingredientPage/icon'
 
 
 interface IngredientHeaderProps {
@@ -13,9 +12,9 @@ const IngredientHeader = ({name, icon}: IngredientHeaderProps) => {
   return (
     <div className='flex items-center gap-3'>
       <Title title={name} />
-      <Icon>
-        {icon}
-      </Icon>
+      <div className='w-15 h-15 flex justify-center items-center rounded-full bg-amber-100 text-3xl'>
+      {icon}
+      </div>
     </div>
   )
 }
