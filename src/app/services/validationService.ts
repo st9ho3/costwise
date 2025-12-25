3c2b01aec528e4534e74d65f2f7bd63f5136d6b3
st9ho3 <stahos21@gmail.com>
2025-12-25 19:09:13 +0100

    feat/suppliers: Remove old code

--- src/app/services/validationService.ts
+++ src/app/services/validationService.ts
@@ -1,35 +1,6 @@
-import { Ingredient, IngredientCategory, IngredientCategorySchema, IngredientSchema, RecipeIngredients, RecipeIngredientsSchema, RecipeSchema, SupplierSchema } from "@/shemas/recipe";
-import { RecipeUpdatePayload, SupplierUpdatePayload } from "@/types/context";
+import { Ingredient, IngredientSchema} from "@/shemas/recipe";
+
 import { z } from "zod";
-
-export const zodValidateDataBeforeAddThemToDatabase = (request: RecipeUpdatePayload) => {
-  const {recipe, addedIngredients, removedIngredients} = request;
-  if (typeof recipe.dateCreated === 'string') {
-    recipe.dateCreated = new Date(recipe.dateCreated);
-  }
-
-  const validatedRecipe = RecipeSchema.parse(recipe);
-
-  let validatedAddedIngredients;
-  let validatedRemovedIngredients;
-
-  if (addedIngredients && addedIngredients.length > 0) {
-    validatedAddedIngredients = addedIngredients.map((ingredient: RecipeIngredients) => {
-      return RecipeIngredientsSchema.parse(ingredient);
-    });
-  }
-  if (removedIngredients && removedIngredients.length > 0) {
-    validatedRemovedIngredients = removedIngredients.map((ingredient: RecipeIngredients) => {
-      return RecipeIngredientsSchema.parse(ingredient);
-    });
-  }
-
-  return {
-    validatedRecipe: validatedRecipe,
-    validatedRecipeAddedIngredients: validatedAddedIngredients ? validatedAddedIngredients : [],
-    validatedRecipeRemovedIngredients: validatedRemovedIngredients ? validatedRemovedIngredients : []
-  };
-};
 
 export const zodValidateIngredientBeforeAddItToDatabase =  (request: Ingredient) => {
   const ingredient = request
@@ -40,39 +11,6 @@ export const zodValidateIngredientBeforeAddItToDatabase =  (request: Ingredient)
       return validatedIngredient;
     }
 };
-
-export const zodValidateSupplierBeforeAddThemToDatabase =(request: SupplierUpdatePayload) => {
-  
-  const {supplier, addedCategories, removedCategories} = request
-
-  if (supplier) {
-    if (typeof supplier.dateAdded === 'string') {
-    supplier.dateAdded = new Date(supplier.dateAdded)
-  }
-}
-    const validatedSupplier = SupplierSchema.parse(supplier)
-
-    let validatedAddedCategories
-    let validatedRemovedCategories
-
-    if (addedCategories && addedCategories.length > 0) {
-     validatedAddedCategories = addedCategories.map((category: IngredientCategory) => {
-      return IngredientCategorySchema.parse(category);
-    });
-  }
-  if (removedCategories && removedCategories.length > 0) {
-     validatedRemovedCategories = removedCategories.map((category: IngredientCategory) => {
-      return IngredientCategorySchema.parse(category);
-      
-    });
-    }
-  
-  return {
-    validatedSupplier: validatedSupplier,
-    validatedAddedCategories: validatedAddedCategories ? validatedAddedCategories : [],
-    validatedRemovedCategories: validatedRemovedCategories ? validatedRemovedCategories : []
-  };
-}
 
 export const validateComplexEntity = <T extends object, TArrayItem>(
   entity: T, 
